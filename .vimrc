8dfde238c6c4130cf3fbde0559a541d078bb3713
Anton Mamaenko <amamaenko@gmail.com>
2017-02-12 20:59:17 +0300

    List folders/files functionality based on Google tutorial

---
+++ .vimrc
@@ -0,0 +1,1 @@
+cd ~/Coding/HelloPy/drive-downloader/
---
+++ dwnldr/helpers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+'''This module contains the helper functions required for interacting with
+Google Drive API
+'''
+
+
+def print_items(items):
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print('{0} ({1})'.format(item['name'], item['id']))
+
+
+def get_folders(service):
+    q = '''
+        mimeType = 'application/vnd.google-apps.folder' and
+        name contains 'Storage'
+    '''
+    request = service.files().list(
+        q=q,
+        pageSize=20
+    )
+    results = request.execute()
+    return results
