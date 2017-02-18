f37370eb46ee11831b910f6791faf926d84ef197
Anton Mamaenko <amamaenko@gmail.com>
2017-02-19 02:15:44 +0300

    Update api and refactoring the main module

--- dwnldr/dwnldr.py
+++ dwnldr/dwnldr.py
@@ -1,54 +1,11 @@
 from __future__ import print_function
 import httplib2
-import os
+import argparse
 
-from apiclient import discovery
-from oauth2client import client
 from oauth2client import tools
-from oauth2client.file import Storage
+from apiclient import discovery
 
-from helpers import find_dir, get_folders, print_items
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Drive Bulk Downloader application'
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'drive-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+import helpers
 
 
 def main():
@@ -57,14 +14,16 @@ def main():
     Creates a Google Drive API service object and outputs the names and IDs
     for up to 20 files.
     """
-    credentials = get_credentials()
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+
+    credentials = helpers.get_credentials(flags)
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
 
     # res = get_folders(service)
-    res = find_dir(service, "Storage Documents")
+    res = helpers.find_dir(service, "Storage Documents")
     items = res.get('files', [])
-    print_items(items)
+    helpers.print_items(items)
 
     '''
     request = service.files().list(
--- dwnldr/helpers.py
+++ dwnldr/helpers.py
@@ -3,6 +3,17 @@
 '''This module contains the helper functions required for interacting with
 Google Drive API
 '''
+import os
+
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive Bulk Downloader application'
 
 
 def print_items(items):
@@ -17,7 +28,7 @@ def print_items(items):
 def find_dir(service, dir_name):
     q = '''
         mimeType = 'application/vnd.google-apps.folder' and
-        name contains '{0}'
+        name = '{0}'
     '''.format(dir_name)
     request = service.files().list(
         q=q, pageSize=20)
@@ -36,3 +47,35 @@ def get_folders(service):
     )
     results = request.execute()
     return results
+
+
+def get_credentials(flags):
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Args:
+        flags: command-line arguments parsed by the argparser
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
