9917788412d76aa46b417137f09fcedfdd61b7ba
Anton Mamaenko <amamaenko@gmail.com>
2017-03-12 13:28:38 +0300

    Improve CLI tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
-"""Tests the basic CLI interface
+# -*- coding: utf8 -*-
+"""Acceptance tests for the CLI interface
 """
 import subprocess
 import os
 
-def test_cli():
-    """Check the simple app launch
+def test_cli_dry_run():
+    """Check the simple app launch with the '--dry-run' flag set.
     """
     print()
     print("Currend working directory: {0}".format(os.getcwd()))
     args = ['python', '-m', 'dwnldr', '--dry-run', '-f', '"Storage Documents"']
-    subprocess.run(args)
+    result = subprocess.run(args)
+    assert result.returncode == 0
